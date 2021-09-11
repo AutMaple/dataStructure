0235a7eb4fa04f34e501f22de26bc4256e0134a7
AutMaple <838498279@qq.com>
2021-09-11 22:27:22 +0800

    easily read code by add comment

--- test.cpp
+++ test.cpp
@@ -3,6 +3,9 @@
 
 class Test{
 public:
+    /*
+     * 测试直接插入算法
+     * */
     static void testDirectInsertSort(){
         int arrSize = 20;
         int* arr = randomArrary(arrSize, 5, 30);
@@ -11,6 +14,9 @@ public:
         printArray(arr, arrSize);
     }
 
+    /*
+     * 测试折半插入算法
+     * */
     static void testBinaryInsertSort(){
         int arrSize = 20;
         int* arr = randomArrary(arrSize, 5, 30);
