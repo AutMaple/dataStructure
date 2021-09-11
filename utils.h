50de95aea8e5d57d9e8e6ee0b982bbdc1491ba82
AutMaple <838498279@qq.com>
2021-09-11 20:59:49 +0800

    工具类

--- utils.h
+++ utils.h
@@ -5,4 +5,14 @@
 #ifndef DATASTRUCTURE_UTILS_H
 #define DATASTRUCTURE_UTILS_H
 
+/*
+ * 打印数组
+ * */
+void printArray(int nums[], int length);
+
+/*
+ * 生成 min ~ max 之间的随机数组成的数组
+ * */
+int* randomArrary(int arrSize, int minNum, int maxNum);
+
 #endif //DATASTRUCTURE_UTILS_H
