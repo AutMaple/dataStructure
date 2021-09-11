2706f6256111c667a43adbcd212fbcafcae5e17a
AutMaple <838498279@qq.com>
2021-09-11 21:00:15 +0800

    工具类的实现

---
+++ utils.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ctime>
+//
+// Created by autmaple on 2021/9/11.
+//
+
+void printArray(int nums[], int length)
+{
+    for(int i = 0; i < length; i++)
+    {
+        std::cout << nums[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int* randomArrary(int arrSize, int minNum, int maxNum)
+{
+    int * arr = new int[arrSize];
+    srand(time(nullptr));
+    for(int i = 0; i < arrSize; i++)
+    {
+        arr[i] = rand() % (maxNum - minNum);
+    }
+    return arr;
+}
